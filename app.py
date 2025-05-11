20945ea8534059527f9d2e7db71380ba7ae4bed0
nihal <nihal@nihals-MacBook-Air.local>
2025-05-11 20:07:58 +0600

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -199,7 +199,5 @@ def clear_files():
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
